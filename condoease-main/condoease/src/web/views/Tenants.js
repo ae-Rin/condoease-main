17213b47923a09a89e86d6238cc2a8ce56adb3bb
Rin <banlosairen@gmail.com>
2025-12-09 20:16:13 +0800

    changes on backend if success

--- condoease-main/condoease/src/web/views/Tenants.js
+++ condoease-main/condoease/src/web/views/Tenants.js
@@ -199,7 +199,7 @@ const Tenants = () => {
         throw new Error(result.detail || 'Unknown error')
       }
 
-      alert('Tenant registered successfully!')
+      alert('Tenant registered successfully YAAAAYYYYY!')
       setFormValues({
         lastName: '',
         firstName: '',
