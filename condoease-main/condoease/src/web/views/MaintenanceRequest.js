f3ef03b62229b3155e1281a40dd5e11e988eed1c
Rin <banlosairen@gmail.com>
2026-01-12 13:08:51 +0800

    up on ui again1

--- condoease-main/condoease/src/web/views/MaintenanceRequest.js
+++ condoease-main/condoease/src/web/views/MaintenanceRequest.js
@@ -229,7 +229,7 @@ const MaintenanceRequest = () => {
                 }}
               >
                 {loading ? (
-                  <CSpinner style={{ width: '0.5rem', height: '0.5rem', color: '#FFFFFF' }} />
+                  <CSpinner style={{ width: '0.1rem', height: '0.1rem', color: '#FFFFFF' }} />
                 ) : (
                   'Send Decision'
                 )}
