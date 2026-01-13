c47144bb7c79ccfd04784b674431cbac078cd07c
Rin <banlosairen@gmail.com>
2026-01-13 20:29:16 +0800

    revert

--- condoease-main/condoease/src/web/views/Announcements.js
+++ condoease-main/condoease/src/web/views/Announcements.js
@@ -280,10 +280,10 @@ const Announcements = () => {
               </div>
               {previewUrl &&
                 (file?.type?.startsWith('image/') ? (
-                  <img src={post.file_url} alt="Preview" width="200" />
+                  <img src={previewUrl} alt="Preview" width="200" />
                 ) : file?.type?.startsWith('video/') ? (
                   <video width="320" height="240" controls>
-                    <source src={post.file_url} type={file.type} />
+                    <source src={previewUrl} type={file.type} />
                     Your browser does not support the video tag.
                   </video>
                 ) : (
